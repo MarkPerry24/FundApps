544eaf0c82fc47f501a2a52cb56892651654f764
Someone24github <38302606+Someone24github@users.noreply.github.com>
2019-01-25 03:00:17 +0000

    Add files via upload

--- RotateLogs.ps1
+++ RotateLogs.ps1
@@ -1,15 +1,25 @@
-﻿param
+﻿<#
+    .SYNOPSIS
+    Script for rotating files older than x days defaults to 7 days.
+#>
+
+param
 (
+    [Parameter(Mandatory)]
     [System.IO.FileInfo]
     $LogFilePath,
 
+    [Parameter(Mandatory)]
     [System.IO.FileInfo]
-    $DestinationPath
+    $DestinationPath,
+
+    [uint16]
+    $Age = 7
 )
 
 # Resolve Paths to stop strange behaviour e.g. with a path of ".\" or "."
 $LogFilePath = Resolve-Path -Path $LogFilePath | Select -ExpandProperty Path
 $DestinationPath = Resolve-Path -Path $DestinationPath | Select -ExpandProperty Path
-Get-ChildItem -LiteralPath $LogFilePath.FullName -File | ? LastWriteTime -lt (Get-Date).AddDays(-7) | %{
+Get-ChildItem -LiteralPath $LogFilePath.FullName -File | ? LastWriteTime -lt (Get-Date).AddDays($Age) | %{
     Move-Item $_ -Destination $DestinationPath
 }
